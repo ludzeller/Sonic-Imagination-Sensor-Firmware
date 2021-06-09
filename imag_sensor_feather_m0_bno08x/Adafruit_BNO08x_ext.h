cad526aa3b4f79f41a397917012c0b9dd1d7be66
Martin Rumori <rumori@iem.at>
2021-06-09 04:16:42 +0200

    added bno08x extension class

---
+++ imag_sensor_feather_m0_bno08x/Adafruit_BNO08x_ext.h
@@ -0,0 +1,25 @@
+/* Adafruit_BNO08x_ext.h
+ * 
+ * extension of Adafruit's BNO08x sensor interface
+ * part of imagination sensor firmware
+ * 
+ * 2021-05 rumori
+ */
+
+#pragma once
+
+#include <Adafruit_BNO08x.h>
+
+class Adafruit_BNO08x_ext : public Adafruit_BNO08x
+{
+  public:  
+    bool softwareReset();
+
+    bool saveDynamicCalibrationData();
+    
+    bool setSensorsPerformingDynamicCalibration (uint8_t sensors);
+
+    uint8_t getSensorsPerformingDynamicCalibration();
+
+
+}; // class Adafruit_BNO08x_ext
