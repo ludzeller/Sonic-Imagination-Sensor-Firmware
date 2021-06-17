555a389bb1e031c6ddbebff8d2cd55841fc623ab
Martin Rumori <martin.rumori@sonible.com>
2021-06-17 18:08:43 +0200

    prepare display and battery state

--- imag_sensor_feather_m0_bno08x/imag_sensor_feather_m0_bno08x.ino
+++ imag_sensor_feather_m0_bno08x/imag_sensor_feather_m0_bno08x.ino
@@ -11,11 +11,18 @@
 #include "imag_imu_bno08x.h"
 #include "imag_net_winc150x.h"
 
+#include <Adafruit_SSD1306.h>
+
 // should go into separate file
 #define BUTTON_A  9
 #define BUTTON_B  6
 #define BUTTON_C  5
-  
+
+// battery status
+#define VBATPIN A6
+
+Adafruit_SSD1306 display { 128, 32, &Wire };   // oled display
+
 // member data
 Imag::Imu_BNO08x imu;
 Imag::Net_WINC150x net;
@@ -37,16 +44,30 @@ void setup()
   DBG(Imag::Config::versionMinor); DBG(".");
   DBGLN(Imag::Config::versionSub);
 
+  // init display: cleanup/separate!
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C); // Address 0x3C for 128x32
+  display.dim(true);
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+
   // init sensor
   if (! imu.init())
   {
     DBGLN("Sensor init failed");
     Imag::Debug::halt();
   }
-	else
-	{
-		imu.printSensorsPerformingDynamicCalibration();
-	}
+
+  // adapt to mounting orientation of sensor
+  //imu.setReorientation (0.0, 0.0, 0.0, 1.0);  // normal, no reorientation
+  //imu.setReorientation (-1.0, 0.0, 0.0, 0.0); // upside down front
+  imu.setReorientation (0.0, -1.0, 0.0, 0.0); // upside down back
+  
+	imu.printSensorsPerformingDynamicCalibration();
+
+  // set data types we are interested in
+  imu.setDataTypesToQuery ({ Imag::Imu::DataType::rotation });
+  //imu.setDataTypesToQuery ({ Imag::Imu::DataType::rotation_arvr });
 
   // init network transport
   if (! net.init (Imag::Config::localIP))
@@ -62,12 +83,12 @@ void loop()
   // update/check current network status
   net.updateConnectionState();
 
-//  if (! net.isConnected())
-//  {
-//    DBGLN("Waiting for wifi connection...");
-//    delay (2000);
-//    return;
-//  }
+  if (! net.isConnected())
+  {
+    DBGLN("Waiting for wifi connection...");
+    delay (2000);
+    return;
+  }
 
 	// check for button press
 	if (digitalRead (BUTTON_A) == LOW)
@@ -80,14 +101,26 @@ void loop()
 		imu.endCalibration();
 		imu.printSensorsPerformingDynamicCalibration();
 	}
-	
+  else if (digitalRead (BUTTON_C) == LOW)
+  {
+    DBGLN("Setting tare of all axes");
+    imu.setTareFull();
+  }
+
   // read sensor data and send
   while (imu.available())
   {
-		imu.read();
+    if (! imu.read())
+    {
+      DBGLN("failure reading sensor data");
+      continue;
+    }
+    auto acc = imu.getCurrentAccuracy();
+    
+    DBGLN(acc);
 
-		//		if (imu.isCalibrating() && imu.getLastDataType() == Imag::Imu::DataType::mag)
-		imu.printCalibrationReliability();
+		if (imu.isCalibrating() && imu.getLastDataType() == Imag::Imu::DataType::mag)
+		  imu.printCalibrationReliability();
 
 		// skip network sending part if disconnected or calibrating
 		if (imu.isCalibrating() || ! net.isConnected())
@@ -108,7 +141,7 @@ void loop()
 				DBGLN("Osc message construction failed: memory issue?");
 			}
   }
-	//  DBGLN("Sensor has no more data for this query loop");
+	DBGLN("Sensor has no more data for this query loop");
   
   // limit querying rate
   delay (5);
