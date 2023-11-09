7fd737e999ed72d1a0c9982e780a8642b1c8f5c6
Martin Rumori <martin.rumori@sonible.com>
2023-11-09 23:43:26 +0100

    Add emacs dir-locals.el for emacs arduino-cli configuration.

---
+++ imag_sensor_feather_m0_bno08x/.dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((arduino-cli-default-fqbn . "adafruit:samd:adafruit_feather_m0")
+	 (arduino-cli-default-port . "/dev/...")))
