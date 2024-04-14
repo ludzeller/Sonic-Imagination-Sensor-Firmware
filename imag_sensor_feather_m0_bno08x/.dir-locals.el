9d9f3f4ad3b53a5a1301bcd97b28dccffeecec96
Martin Rumori <martin.rumori@sonible.com>
2024-04-14 15:10:37 +0200

    dir-locals.el: Update c++ style and arduino-cli configuration.

--- imag_sensor_feather_m0_bno08x/.dir-locals.el
+++ imag_sensor_feather_m0_bno08x/.dir-locals.el
@@ -1,5 +1,24 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((arduino-cli-default-fqbn . "adafruit:samd:adafruit_feather_m0")
-	 (arduino-cli-default-port . "/dev/...")))
+((auto-mode-alist . (("\\.h\\'" . c++-mode)
+		     ("\\.ino\\'" . c++-mode)))
+ (c++-mode . (
+	      ;; c++-mode coding style
+	      (c-default-style . "linux")
+	      (c-basic-offset . 4)
+	      (indent-tabs-mode . nil)
+	      (eval . (progn
+			(c-set-offset 'innamespace '-)
+			(c-set-offset 'inline-open '0)
+			(c-set-offset 'substatement-open '0)
+			(c-set-offset 'brace-list-open '0)
+			(c-set-offset 'inher-cont 'c-lineup-multi-inher)
+			(c-set-offset 'arglist-cont-nonempty 'c-lineup-arglist-intro-after-paren)
+			(c-set-offset 'template-args-cont '+)
+			))
+
+	      ;; arduino-cli configuration
+	      (eval . (arduino-cli-mode))
+	      (arduino-cli-default-fqbn . "adafruit:samd:adafruit_feather_m0")
+	      (arduino-cli-default-port . "/dev/cu.usbmodem124301"))))
