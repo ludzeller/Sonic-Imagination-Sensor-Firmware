b4bc4a452dcd3a7a0e595df746286217000a35e4
Martin Rumori <martin.rumori@sonible.com>
2024-04-14 15:09:47 +0200

    Adapt to refactored classes.

--- imag_sensor_feather_m0_bno08x/imag_config.h
+++ imag_sensor_feather_m0_bno08x/imag_config.h
@@ -3,11 +3,10 @@
  * imagination sensor firmware
  * global configuration
  * 
- * 2021-05 rumori
+ * 2021-2024 rumori
  */
 
-#ifndef IMAG_CONFIG_H_INCLUDED
-#define IMAG_CONFIG_H_INCLUDED
+#pragma once
 
 #include <Arduino.h>
 #include <array>
@@ -17,42 +16,43 @@
 #define IMAG_IMU_DEBUG 0 // Imu low-level debug
 #define IMAG_NET_DEBUG 0 // Wifi low-level debug
 
-namespace Imag
+namespace imag::config
 {
-namespace Config
+// version information
+static constexpr auto versionMajor = 0;
+static constexpr auto versionMinor = 5;
+static constexpr auto versionSub   = 0;
+  
+// seconds to wait at bootup for serial debug console
+static constexpr auto serialTimeout = 5;
+  
+// serial baudrate
+static constexpr auto serialBaudrate = 115200;
+
+// network configuration
+struct Net
 {
-  // version information
-  static constexpr auto versionMajor = 0;
-  static constexpr auto versionMinor = 0;
-  static constexpr auto versionSub   = 1;
-  
-  // seconds to wait at bootup for serial debug console
-  static constexpr auto serialTimeout = 5;
-  
-  // serial baudrate
-  static constexpr auto serialBaudrate = 115200;
+    static constexpr std::array<byte, 4> localIP { 192, 168, 1, 1 }; // local ip address
+    static constexpr short localPort = 9336; // local (listening) port
 
-  // default local ip address
-  static constexpr std::array<byte, 4> localIP { 192, 168, 1, 1 };
+    static constexpr std::array<byte, 4> remoteIP { 192, 168, 1, 100 }; // target ip address
+    static constexpr auto remotePort = 9336; // target port
+};
 
-  // default local (listening) port
-  static constexpr auto localPort = 9336;
-    
-  // default target ip address
-  static constexpr std::array<byte, 4> remoteIP { 192, 168, 1, 100 };
+// wifi configuration
+struct WiFi
+{
+    static constexpr auto ssid = "imagination";
+    static constexpr auto key = "atmospheres";
+    static constexpr uint8_t channel = 1;
+};
 
-  // default target port
-  static constexpr auto remotePort = 9336;
+// bno08x hardware configuration
+struct BNO08x
+{
+static constexpr uint8_t i2cAddr = 0x4b; // adafruit breakout: 0x4a, slimevr: 0x4b
+static constexpr uint8_t intPin = 11;
+static constexpr uint8_t resetPin = 12;
+};
 
-  // default wifi ap ssid and key
-  static constexpr auto wifiSSID = "imagination";
-  static constexpr auto wifiPASS = "atmospheres";
-
-  // bno08x pins
-  static constexpr auto bno08x_int_pin = 11;
-  static constexpr auto bno08x_reset_pin = 12;
-}
-}
-
-
-#endif // #ifndef IMAG_CONFIG_H_INCLUDED
+} // namespace imag::config
