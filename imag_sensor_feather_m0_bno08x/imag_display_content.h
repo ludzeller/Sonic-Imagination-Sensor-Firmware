6e47a14e4bb419cc72564539739f1041dee9f0e6
Martin Rumori <martin.rumori@sonible.com>
2024-04-16 10:37:06 +0200

    Add SH1107 display interface class.

---
+++ imag_sensor_feather_m0_bno08x/imag_display_content.h
@@ -0,0 +1,119 @@
+/* imag_display_content.h
+ * 
+ * imagination sensor firmware
+ * display messages and content container
+ * 
+ * 2021-2024 rumori
+ */
+
+#pragma once
+
+#include <array>
+
+#include <Arduino_Helpers.h>
+#include <AH/Math/Quaternion.hpp>
+
+namespace imag::display
+{
+// content page selector
+enum class Page
+{
+    none = 0x0,
+    splash,
+    main,
+    calibration
+};
+
+// display message strings
+struct Message
+{
+    // generic
+    static constexpr auto dot = ".";
+    static constexpr auto asterisk = "*";
+    static constexpr auto underscore = "_";
+
+    // splash screen
+    struct Splash
+    {
+        static constexpr auto title = "ImagSens";
+        static constexpr auto versionLabel = "version ";
+        static constexpr auto debugSerialWait = "Waiting for serial...";
+        static constexpr auto initialising = "Initialising...";
+    };
+
+    // main
+    struct Main
+    {
+        // button labels
+        static constexpr std::array<const char*, 3> buttons {
+            "Config", // button C
+            "North",  // button B
+            "Screen"  // button A
+        };
+
+        // orientation mode indicators
+        static constexpr std::array<const char*, 2> orientationConfig {
+            "[imag]", // imagination
+            "[iem]"  // iem MrHat compatible
+        };
+
+        // north indicators
+        static constexpr auto magneticNorth = "[magn]";
+        static constexpr auto customNorth = "[cstm]";
+
+        // connection/sender labels
+        static constexpr auto senderOsc = "OSC";
+        static constexpr auto senderMidi = "MIDI";
+
+        // reliability/accuracy indicators
+        static constexpr auto reliability = "Acc";
+        static constexpr auto accuracy = "Err";
+    };
+
+    struct Battery
+    {
+        // battery label
+        static constexpr auto label = "Bat ";
+    };
+
+    struct Calibration
+    {
+        // label
+        static constexpr auto label = "Calibrating...";
+    
+        // button labels
+        static constexpr std::array<const char*, 3> buttons {
+            "Store", // button C
+            "Cancel",  // button B
+            "Clear"  // button A
+        };
+
+        // calibration reliability indicator
+        static constexpr auto reliability = "Accuracy";
+    };
+};
+
+
+// container for data content to display
+struct Content
+{
+    const char* version;
+
+    const char* ssid;
+    
+    uint8_t orientationConfig = 0;
+
+    bool customNorth = false;
+    
+    bool senderOsc = false;
+    bool senderMidi = false;
+    
+    float reliability = 0.0f;
+    float accuracy = 0.0f;
+
+    Quaternion rotation;
+
+    uint8_t battery = 0;
+};
+
+} // namespace imag::display
---
+++ imag_sensor_feather_m0_bno08x/imag_display_sh1107.h
@@ -0,0 +1,84 @@
+/* imag_display_sh110x.h
+ * 
+ * imagination sensor firmware
+ * sh110x 128x64 display interface
+ * 
+ * 2021-2024 rumori
+ */
+
+#pragma once
+
+#include <Adafruit_SH110X.h>
+
+#include "imag_display_content.h"
+
+namespace imag::display
+{
+
+class SH1107
+{
+public:
+    // display dimensions
+    static constexpr auto displayWidth = 128;
+    static constexpr auto displayHeight = 64;
+
+    // display timings [ms]
+    static constexpr auto displayRefresh = 250UL;
+    static constexpr auto displayAutoOff = 60000UL;
+
+    // constructor
+    SH1107 (TwoWire* twi);
+
+    // initialise display connection
+    bool init (uint8_t i2cAddr, bool reset = true);
+
+    // select content page to display
+    void setPage (Page pageToDisplay) { page = pageToDisplay; }
+
+    // call periodically to check for display refresh/auto-off
+    void update();
+
+    // switch display on or off
+    // returns whether the state was actually changed
+    // restart auto off cycle in any case
+    bool setEnabled (bool shouldBeOn);
+
+    // query display enablement state
+    bool isEnabled() const { return displayOn; }
+
+    // get a content reference
+    Content& getContent() { return content; }
+    
+    // restart auto-off timeout
+    void resetAutoOff() { autoOffTime = millis() + displayAutoOff; }
+
+private:
+    // show splash screen
+    void showSplash();
+
+    // show main content page
+    void showMain();
+    
+    // show calibration page
+    void showCalibration();
+    
+    // display object
+    Adafruit_SH1107 display;
+
+    // content member
+    Content content;
+
+    // page to show
+    Page page;
+    
+    // display enablement flag
+    bool displayOn;
+
+    // refresh time
+    uint32_t refreshTime;
+
+    // auto off time
+    uint32_t autoOffTime;
+};
+
+} // namespace imag::display
