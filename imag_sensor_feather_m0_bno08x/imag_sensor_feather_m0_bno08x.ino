775bb299c6c351bd12104ce24fb2512cf2fb3c77
Martin Rumori <martin.rumori@sonible.com>
2024-04-16 10:40:52 +0200

    Use refactored display and battery service classes.

--- imag_sensor_feather_m0_bno08x/imag_sensor_feather_m0_bno08x.ino
+++ imag_sensor_feather_m0_bno08x/imag_sensor_feather_m0_bno08x.ino
@@ -10,67 +10,55 @@
 #include "imag_debug.h"
 #include "imag_osc_address.h"
 #include "imag_smoother.h"
+#include "imag_battery.h"
 
 #include "imag_imu_bno08x.h"
 #include "imag_osc_winc150x.h"
+#include "imag_display_sh1107.h"
 
 #include <MIDIUSB.h>
-
-#include <Adafruit_SSD1306.h> // 128x32 oled display
-#include <Adafruit_SH110X.h>  // 128x64 oled display
 #include <EasyButton.h>
 
-// #include <ArduinoLowPower.h>
+// string constants
+static const String versionString { String (imag::config::versionMajor) + "." + imag::config::versionMinor + "." + imag::config::versionSub };
+static const String ssid { String (imag::config::WiFi::ssid) + "_" + imag::config::sensorIndex };
 
-// should go into separate file
-#define BUTTON_A  9
-#define BUTTON_B  6
-#define BUTTON_C  5
-
-// battery status
-#define VBATPIN A7 // == D9, so be aware of concurrency w/ button A
-
-// member data
+// members
 imag::imu::BNO08x imu { imag::config::BNO08x::resetPin, imag::config::BNO08x::intPin };
-
 
 const auto localAddr = imag::config::Net::localIP;
 imag::osc::WINC150x net { localAddr, imag::config::Net::localPort };
 
-//Adafruit_SSD1306 display { 128, 32, &Wire };  // oled display 128x32
-Adafruit_SH1107 display { 64, 128, &Wire };   // oled display 128x64
+imag::display::SH1107 oled { &Wire };
 
-EasyButton displayButton(BUTTON_A, 35, true, true);
-EasyButton tareButton(BUTTON_B, 35, true, true);
-EasyButton calibButton(BUTTON_C, 35, true, true);
+imag::Battery battery { imag::config::Battery::pin, true };
 
-static constexpr auto displayAutoOff = 60000UL;
-auto displayTimeOut = millis() + displayAutoOff;
+// buttons
+EasyButton buttonA (imag::config::Button::pinA, imag::config::Button::debounce, true, true);
+EasyButton buttonB (imag::config::Button::pinB, imag::config::Button::debounce, true, true);
+EasyButton buttonC (imag::config::Button::pinC, imag::config::Button::debounce, true, true);
+
+// convenience array
+static const std::array<EasyButton*, 3> buttons { { &buttonA, &buttonB, &buttonC } };
 
 // sensor data types
 static constexpr auto primaryDataType = imag::imu::DataType::rotationArvr;
 
 // sensor mounting orientation/output conventions
-static std::array<Quaternion, 2> sensorOrientations {
+static const std::array<const Quaternion, 2> sensorOrientations {
     {
         { 1.0f, 0.0f, 0.0f, 0.0f },             // normal mounting, no reorientation
-        { 0.7071068f, 0.0f, 0.0f, -0.7071068f }  // normal mounting, no reorientation w/ iem mrhat compat rotation
+        { 0.7071068f, 0.0f, 0.0f, -0.7071068f } // normal mounting, no reorientation w/ iem mrhat compat rotation
         // { 0.0f, -1.0f, 0.0f, 0.0f },         // mounted upside down front
         // { 0.0f, 0.0f, -1.0f, 0.0f },         // mounted upside down back : imagination prototype mounting
         // { 0.0f, -0.7071f, 0.7071f, 0.0f }    // mounted upside down back w/ iem mrhat compat rotation
     }
 };
 
-// orientation mode indicators
-static constexpr std::array<const char*, 2> sensorOrientationModes {
-    "[std]", // imagination
-    "[iem]"  // iem MrHat compatible
-};
-
-static_assert (sensorOrientations.size() == sensorOrientationModes.size());
+static_assert (sensorOrientations.size() == imag::display::Message::Main::orientationConfig.size());
 
 // current orientation mode
-int orientationMode = 0;
+uint8_t orientationMode = 0;
 
 // custom north offset
 bool customNorth = false;
@@ -80,76 +68,61 @@ Quaternion customNorthOffset = Quaternion::identity();
 static constexpr auto smoothLen = 100;
 static imag::Smoother<float, smoothLen> reliability, accuracy;
 
-// display state
-auto displayStateOn = true;
+void attachButtonsNorm();
+void attachButtonsCalibration();
 
-// int flag
-auto intHigh = true;
-
-
-void resetDisplayTimeout()
+void beginCalibration()
 {
-    displayTimeOut = millis() + displayAutoOff;  
-}
+    if (oled.setEnabled (true))
+        return;
 
-
-void displayOff()
-{
-    displayStateOn = false;
-    display.clearDisplay();
-    display.display();
-}
-
-
-void displayOn()
-{
-    displayStateOn = true;
-    resetDisplayTimeout();
-}
-
-
-void toggleDisplay()
-{
-    displayStateOn ? displayOff() : displayOn();
-}
-
-
-void beginOrCancelCalibration()
-{
-    displayOn();  
-    if (imu.isCalibrating())
-        imu.endCalibration();
-    else
+    if (! imu.isCalibrating())
         imu.beginCalibration();
 
     imu.printSensorsPerformingDynamicCalibration();
+    attachButtonsCalibration();
 }
 
 
-void switchModeOrEndCalibration()
+void endCalibration()
 {
-    // reset display timeout
-    displayOn();
+    if (imu.isCalibrating())
+        imu.endCalibration();
 
-    if (! imu.isCalibrating())
-    {
-        // rotate mode
-        orientationMode = ++orientationMode % sensorOrientations.size();
-        imu.setReorientation (sensorOrientations[orientationMode]);
-    
-        return;
-    }
-  
-    // end calibration
-    imu.endCalibration();
-    imu.saveCalibration();
     imu.printSensorsPerformingDynamicCalibration();
+    attachButtonsNorm();
 }
 
 
-void setTare()
+void saveCalibration()
 {
-    displayOn();
+    endCalibration();
+    imu.saveCalibration();
+}
+
+
+void clearCalibration()
+{
+    endCalibration();
+    imu.clearCalibration();
+}
+
+
+void cycleOrientationMode()
+{
+    if (oled.setEnabled (true))
+        return;
+
+    // rotate mode
+    orientationMode = ++orientationMode % sensorOrientations.size();
+    imu.setReorientation (sensorOrientations[orientationMode]);
+}
+
+
+void setReorientation()
+{
+    if (oled.setEnabled (true))
+        return;
 
     if (imag::imu::isAnyRotationDataType (imu.getLastDataType()))
     {
@@ -166,54 +139,67 @@ void setTare()
 }
 
 
-void resetTare()
+void resetReorientation()
 {
-    displayOn();
+    if (oled.setEnabled (true))
+        return;
   
     customNorthOffset = Quaternion::identity();
     customNorth = false;
 }
 
 
+void toggleDisplay()
+{
+    oled.setEnabled (! oled.isEnabled());
+}
+
+
+void nop() {}
+
+
+// attach buttons, normal mode
+void attachButtonsNorm()
+{
+    buttonA.onPressed (toggleDisplay);
+
+    buttonB.onPressed (setReorientation);
+    buttonB.onPressedFor (2000, resetReorientation);
+
+    buttonC.onPressed (cycleOrientationMode);
+    buttonC.onPressedFor (2000, beginCalibration);
+}
+
+
+// attach buttons, calibration mode
+void attachButtonsCalibration()
+{
+    buttonA.onPressed (clearCalibration);
+
+    buttonB.onPressed (endCalibration);
+    buttonB.onPressedFor (2000, nop);
+
+    buttonC.onPressed (saveCalibration);
+    buttonC.onPressedFor (2000, nop);
+}
+
+
 void setup()
 {
-    // delay (500); // give the baby some time to settle
+    delay (500); // give the baby some time to settle
 
-    //Wire.setClock(200000L);   // I2C speed, 400 kHz is a little fast for Arduino's pullups
+    Wire.setClock(200000L);   // I2C speed, 400 kHz is a little fast for Arduino's pullups
 
     // led
     pinMode (LED_BUILTIN, OUTPUT);
     digitalWrite (LED_BUILTIN, LOW);
 
-    // init display: cleanup/separate!
-    // 128x32
-    // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-    /*
-      display.begin (SSD1306_SWITCHCAPVCC, 0x3C);
-      delay (100);
-      display.dim (true);
-      display.setTextColor (SSD1306_WHITE);
-    */  
-  
-    // 128x64
-    display.begin (0x3C, true);
-    display.setRotation (1);
-    display.setTextColor (SH110X_WHITE);
-
-    display.clearDisplay();
-    display.setCursor (0, 0);
-    display.setTextSize (2);
-    display.println ("ImagSens");
-    display.setTextSize (1);
-    display.println();
-
-#if IMAG_DEBUG
-    display.println ("Waiting for serial...");
-#else
-    display.println ("Initialising...");
-#endif // IMAG_DEBUG
-
-    display.display();
+    // oled display
+    oled.init (imag::config::Display::i2cAddr);
+    oled.getContent().version = versionString.c_str();
+    oled.getContent().ssid = ssid.c_str();
+    oled.setPage (imag::display::Page::splash);
+    oled.update();
 
     // init debug serial console in case debugging is enabled
     imag::Debug::init();
@@ -241,7 +227,7 @@ void setup()
     }
 
     // init network transport
-    if (! net.init (imag::config::WiFi::ssid, imag::config::WiFi::key, imag::config::WiFi::channel))
+    if (! net.init (ssid.c_str(), imag::config::WiFi::key, imag::config::WiFi::channel))
     {
         DBGLN("Starting wifi failed");
         imag::Debug::halt();
@@ -250,39 +236,20 @@ void setup()
     net.setTarget (imag::config::Net::remoteIP, imag::config::Net::remotePort);
 
     // init buttons
-    displayButton.begin();
-    displayButton.onPressed (toggleDisplay);
+    for (auto* button : buttons)
+        button->begin();
 
-    tareButton.begin();
-    tareButton.onPressed (setTare);
-    tareButton.onPressedFor (2000, resetTare);
-
-    calibButton.begin();
-    calibButton.onPressed (switchModeOrEndCalibration);
-    calibButton.onPressedFor (2000, beginOrCancelCalibration);
+    attachButtonsNorm();
 }
 
 
 void loop()
 {
     auto now = millis();
-    static auto displayRateTime = now;
     static auto connMsgTime = now;
 
     // flag for any received data
     auto dataReceived = false;
-
-    // check for int line
-    // if (digitalRead (imag::config::BNO08x::intPin) == LOW && intHigh == true)
-    // {
-    // intHigh = false;
-    // DBGLN ("INT falling edge");
-    // }
-    // else if (digitalRead (imag::config::BNO08x::intPin) == HIGH && intHigh == false)
-    // {
-    // intHigh = true;
-    // DBGLN ("INT rising edge");
-    // }
 
     // update/check current network status
     net.updateConnectionState();
@@ -295,13 +262,15 @@ void loop()
     }
 
     // eval buttons
-    displayButton.read();
-    tareButton.read();
-    calibButton.read();
+    for (auto* button : buttons)
+        button->read();
 
-    // display timeout
-    if (displayStateOn && now > displayTimeOut)
-        displayOff();
+    // display refresh/timeout
+    oled.setPage (imu.isCalibrating() ? imag::display::Page::calibration : imag::display::Page::main);
+    oled.update();
+
+    // battery read cycle if due
+    battery.update();
 
     // read sensor data and send
     while (imu.read())
@@ -357,6 +326,13 @@ void loop()
             // if (! success)
             //     DBGLN("Error sending MIDI data");
 
+            // update display data
+            oled.getContent().orientationConfig = orientationMode;
+            oled.getContent().customNorth = customNorth;
+            oled.getContent().rotation = rot;
+            oled.getContent().senderOsc = net.isReadyToSend();
+            oled.getContent().senderMidi = success;
+            
             // skip network sending part if disconnected or calibrating
             if (imu.isCalibrating() || ! net.isReadyToSend())
                 continue;
@@ -372,55 +348,17 @@ void loop()
     }
     // DBGLN("Sensor has no more data for this query loop");
 
-    // time for display update?
-    if (displayStateOn && now > displayRateTime)
-    {
-        // measure battery voltage
-        pinMode(BUTTON_A, INPUT); // disable pullup for button, read voltage
-        float battery = analogRead(VBATPIN);
-        pinMode(BUTTON_A, INPUT_PULLUP); // re-enable pullup for button
-        battery *= 2.0f;    // we divided by 2, so multiply back
-        battery *= 3.3f;  // Multiply by 3.3V, our reference voltage
-        battery /= 1024.0f; // convert to voltage
-        DBG("Battery: "); DBGNLN(battery);
+    { // update remaining display data
+        oled.getContent().battery = battery.getPercentage();
 
-        // calculate effective reliability/accuracy to display
-        auto rel = reliability.get();
-        auto acc = constrain (accuracy.get() * 180.0 / PI, 0.0, 23.0);
+        oled.getContent().reliability = reliability.get();
+        oled.getContent().accuracy = constrain (accuracy.get(), 0.0f, 0.5f * PI) / (0.5f * PI); // constrain to 0..90 deg
 
-        DBG("smoothed reliability: "); DBGNLN(rel);
-        DBG("smoothed accuracy: "); DBGNLN(acc);
-
-        // display
-        display.clearDisplay();
-        display.setTextSize(1);
-        display.setCursor (0, 0);
-        display.print ("ImagSens "); display.print (sensorOrientationModes[orientationMode]); display.println (" ac  re");
-        display.print ("Battery: "); display.print (battery, 1); display.println ("V");
-        display.println (customNorth ? "[Custom North]" : "");
+        DBG("smoothed reliability: "); DBGNLN(oled.getContent().reliability);
+        DBG("smoothed accuracy: "); DBGNLN(oled.getContent().accuracy * 90.0f);
 
         if (imu.isCalibrating())
-	{
-            display.println("Calibrating...");
-            display.drawRect (88, 8, 16, 24, SSD1306_WHITE); // empty accuracy
-
-            // reset display timeout while calibrating
-            resetDisplayTimeout();
-	}
-        else
-	{
-            display.println (net.isReadyToSend() ? "Sending" : "Disconnected");
-
-            // reported accuracy
-            display.fillRect (88, 8, 16, int (acc) + 1, SSD1306_WHITE);
-	}
-
-        // reliability
-        display.fillRect (112, 32 - int (rel * 23) - 1, 16, int (rel * 23) + 1, SSD1306_WHITE);
-        display.display();
-
-        // next display refresh
-        displayRateTime += 200;
+            oled.resetAutoOff(); // do not auto-off when calibrating
     }
 
     // wait for sensor interrupt
@@ -439,12 +377,4 @@ void loop()
     //     DBG("data received. ");
     // DBG("loop took [ms]: "); DBGNLN(elapsed);
 
-    // if (dataReceived && elapsed < 10)
-    // {
-    //     // sleep until next tick of our sensor rate (100 Hz for now)
-    //     // waking up seems to take way too long - not usable atm.
-    //     //LowPower.idle (10 - elapsed);
-
-    //     delay (10 - elapsed);
-    // }
 }
